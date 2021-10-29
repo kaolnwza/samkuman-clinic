973745c540f379cb82e636c4c3ca931cc4afd15a
PwrFr <54875724+PwrFr@users.noreply.github.com>
2021-10-29 17:28:38 +0700

    login page (mock up)

---
+++ Frontend/screen/login.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { StyleSheet, Text, View, ImageBackground, TextInput, Button } from 'react-native'
+import { SvgXml } from 'react-native-svg';
+import Bh1 from '../assets/background/normal_u12.svg'
+
+const login = () => {
+    return (
+        <View style={{ flex: 1 }}>
+            <Text>Login</Text>
+            <TextInput
+                placeholder="User"
+            />
+            <TextInput
+                placeholder="Password"
+            />
+            <Button title='Login' />
+        </View>
+    )
+}
+
+export default login
+
+const styles = StyleSheet.create({
+
+})
