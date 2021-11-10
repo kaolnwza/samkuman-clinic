c6ae5a0830c92ba28765b5e381d7de13181844f7
Nutchayaporn <54405491+Nutchayaporn@users.noreply.github.com>
2021-11-11 02:19:42 +0700

    Noti(Not Complete)

--- FrontEnd/screen/Notify.js
+++ FrontEnd/screen/Notify.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList, } from 'react-native'
 import Bg from '../components/Pagebg'
+import NotiGridTile from '../components/NotiGridTile';
+import { useFonts } from 'expo-font';
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 const DATA1 = [
    {
         title: 'Notification 1',
-        info: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum        ",
+        info: "Lorem Ipsum is  it",
     }, 
     {
         title: 'Notification 2',
-        info: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum        ",
+        info: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap inLorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap inLorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap inLorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap inLorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum        ",
     },
     {
         title: 'Notification 3',
@@ -39,12 +42,37 @@ const DATA1 = [
 
 ];
 
-const Notify = () => {
-    return (
+const Notify = ({navigation}) => {
+    const renderGridItem = (itemData) => {
+        return (
+          <NotiGridTile
+          style={{ height: "50", }}
+            title={itemData.item.title}
+            info={itemData.item.info}
+            // color={itemData.item.color}
+            // onSelect={() => {
+            //   // เขียนโค้ดเพิ่ม
+            //   navigation.navigate("CategoryMeals", {categoryId : itemData.item.id , categoryTitle : itemData.item.title})
+            // }}
+          />
+    
+        );
+      };
+
+
+      return (
+        
+
+          
         <View style={styles.container}>
             <Bg Text1='Notification' />
+            <FlatList style={styles.position} data={DATA1} renderItem={renderGridItem} numColumns={1} />
         </View>
-    )
+    
+      );
+
+
+    
 }
 
 export default Notify
@@ -55,4 +83,7 @@ const styles = StyleSheet.create({
         width: '100%',
         backgroundColor: '#6488e4',
     },
+    position: {
+         transform: [{ translateY: RFPercentage(-45) }]
+    }
 })
---
+++ Frontend/components/NotiGridTile.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import { useFonts } from 'expo-font';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { RFPercentage } from 'react-native-responsive-fontsize';
+
+
+const NotiGridTile = (props) => {
+  return (
+    <TouchableOpacity
+      style={styles.gridItem}
+    //   onPress={() => {
+    //     props.onSelect();
+    //   }}
+    >
+      <View
+        // style={{ ...styles.container, ...{ backgroundColor: props.color } }}
+        style={styles.container}
+      >
+        {/* <Text>{itemData.item.title}</Text> */}
+        <Text style={styles.title} numberOfLines={2}> 
+          {props.title}
+        </Text>
+        <Text style={styles.info} > 
+          {props.info}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    marginHorizontal: RFPercentage(4),
+    marginVertical: RFPercentage(1),
+  },
+  container: {
+    flex: 1,
+    borderRadius: RFPercentage(1),
+    shadowColor: "black",
+    shadowOpacity: RFPercentage(1),
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: RFPercentage(0.5),
+    elevation: 3,
+    padding: RFPercentage(1.2),
+    backgroundColor: "#fff9ec",
+  },
+
+  title: {
+    fontFamily: 'Poppins',
+    fontSize: RFPercentage(2),
+    fontWeight: "bold",
+  },
+
+  info: {
+    fontFamily: 'Poppins',
+    fontSize: RFPercentage(1.2),
+  }
+});
+
+export default NotiGridTile;
