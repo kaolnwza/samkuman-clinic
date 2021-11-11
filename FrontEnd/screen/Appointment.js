2cfd1d54d0fd63e3865bc919378a4007f2b91eae
PwrFr <54875724+PwrFr@users.noreply.github.com>
2021-11-12 00:57:11 +0700

    fixed Notification

--- FrontEnd/screen/Appointment.js
+++ FrontEnd/screen/Appointment.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Bg from '../components/Pagebg'
-
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 const Appointment = () => {
     return (
         <View style={styles.container}>
             <Bg Text1='Appointment' />
-
+            <View style={styles.position}>
+                <Text>asfaf</Text>
+            </View>
         </View>
     )
 }
@@ -18,5 +21,11 @@ const styles = StyleSheet.create({
         flex: 1,
         width: '100%',
         backgroundColor: '#6488e4',
+    },
+    position: {
+        flexDirection: 'column',
+        alignSelf: 'center',
+        position: 'absolute',
+        transform: [{ translateY: RFPercentage(25) }]
     },
 })
--- FrontEnd/screen/Notify.js
+++ FrontEnd/screen/Notify.js
@@ -4,12 +4,14 @@ import Bg from '../components/Pagebg'
 import NotiGridTile from '../components/NotiGridTile';
 import { useFonts } from 'expo-font';
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 const DATA1 = [
-   {
+    {
         title: 'Notification 1',
         info: "Lorem Ipsum is  it",
-    }, 
+    },
     {
         title: 'Notification 2',
         info: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap inLorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap inLorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap inLorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap inLorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum        ",
@@ -38,41 +40,45 @@ const DATA1 = [
         title: 'Notification 8',
         info: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum        ",
     },
-    
+
 
 ];
 
-const Notify = ({navigation}) => {
+const Notify = ({ navigation }) => {
     const renderGridItem = (itemData) => {
         return (
-          <NotiGridTile
-          style={{ height: "50", }}
-            title={itemData.item.title}
-            info={itemData.item.info}
+            <NotiGridTile
+                // style={{ height: "50", }}
+                title={itemData.item.title}
+                info={itemData.item.info}
             // color={itemData.item.color}
             // onSelect={() => {
             //   // เขียนโค้ดเพิ่ม
             //   navigation.navigate("CategoryMeals", {categoryId : itemData.item.id , categoryTitle : itemData.item.title})
             // }}
-          />
-    
+            />
+
         );
-      };
+    };
 
 
-      return (
-        
+    return (
 
-          
+
+
         <View style={styles.container}>
             <Bg Text1='Notification' />
-            <FlatList style={styles.position} data={DATA1} renderItem={renderGridItem} numColumns={1} />
+            <View style={styles.position}>
+                <KeyboardAwareScrollView style={{ height: hp('70%') }}>
+                    <FlatList data={DATA1} renderItem={renderGridItem} keyExtractor={item => item.title} numColumns={1} />
+                </KeyboardAwareScrollView>
+            </View>
         </View>
-    
-      );
+
+    );
 
 
-    
+
 }
 
 export default Notify
@@ -84,6 +90,9 @@ const styles = StyleSheet.create({
         backgroundColor: '#6488e4',
     },
     position: {
-         transform: [{ translateY: RFPercentage(-45) }]
+        flexDirection: 'column',
+        alignSelf: 'center',
+        position: 'absolute',
+        transform: [{ translateY: RFPercentage(20) }]
     }
 })
