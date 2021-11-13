363d5bb427d1dcb0193922e753c774ecf1f47cdf
kaolnwza <kaow_jijuy999@hotmail.com>
2021-11-13 12:23:01 +0700

    Create Login.js

---
+++ Frontend/screen/Login.js
@@ -0,0 +1,158 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, Image, KeyboardAvoidingView } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { useFonts } from 'expo-font';
+import { FontAwesome } from '@expo/vector-icons';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Btn from '../components/Button';
+
+const login = ({ navigation }) => {
+<<<<<<< HEAD
+    const [usernameLogin, setUsernameLogin] = useState("12345678912345")
+    const [passwordLogin, setPasswordLogin] = useState("12345")
+
+    const testLogin = () => {
+        console.log(usernameLogin, passwordLogin);
+        postData()
+    }
+    const local = "http://localhost:12345"
+
+    const instance = axios.create({
+        withCredentials: true
+    })
+
+
+    const postData = async () => {
+        const headers = {
+            "Content-Type": "application/json",
+        };
+
+        console.log("send");
+        const data = {
+            identity_number: usernameLogin,
+            password: passwordLogin
+        }
+        await instance
+            .post(local + "/login", data)
+            .then((res) => {
+                console.log(res.data)
+            }
+            )
+        await instance.get(local + "/getcookie")
+            .then(
+                console.log("done get cookie")
+            )
+
+    }
+
+    const Logout = async () => {
+        await instance
+            .get(local + "/logout")
+            .then((res) => {
+                console.log(res.data);
+            })
+    }
+
+=======
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+>>>>>>> a2edee9d10a88bc402456e15e5acf057e8e937b0
+    const [loaded] = useFonts({
+        Poppins: require('../assets/fonts/Poppins-Bold.ttf'),
+    });
+    if (!loaded) {
+        return null;
+    }
+
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.image, styles.im3}>
+                    <Image source={require('../assets/normal_u12.png')} />
+                </View>
+                <View style={styles.image, styles.im2}>
+                    <Image source={require('../assets/normal_u13.png')} />
+                </View>
+                <View style={styles.image}>
+                    <Image source={require('../assets/normal_u15.png')} />
+                </View>
+                <Text style={styles.headerText}>SIGN IN
+                    <FontAwesome name="sign-in" size={50} color="white" />
+                </Text>
+            </View>
+            <KeyboardAwareScrollView style={styles.containerinput} viewIsInsideTabBar={true} extraScrollHeight={-40}>
+                <Text style={styles.label}>Username</Text>
+                <TextInput style={styles.input} placeholder="username" value={username} />
+                <Text style={styles.label}>Password</Text>
+                <TextInput style={styles.input} placeholder="password" value={password} secureTextEntry={true} />
+                <View style={{ marginTop: 20 }}>
+                    <TouchableOpacity style={{ alignItems: 'flex-end' }}
+                        onPress={() => {
+                            navigation.navigate("signup")
+                        }}
+                    >
+                        <Text style={{ color: '#007AFF' }}>Sign Up</Text>
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAwareScrollView>
+            <Btn navigation={navigation} label='LOGIN' color='#f9be7c' to='main' />
+        </View >
+    )
+}
+
+export default login
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#FFF9EC',
+    },
+    containerinput: {
+        flex: 1,
+        paddingHorizontal: "12%",
+        fontFamily: 'Poppins',
+    },
+    input: {
+        fontSize: 18,
+        height: 40,
+        padding: 10,
+        borderBottomWidth: 1,
+        shadowColor: "#000",
+        shadowOffset: { height: 7, width: 0 }, // IOS
+        shadowOpacity: 0.2, // IOS
+        shadowRadius: 3,
+    },
+    label: {
+        fontSize: 15,
+        marginTop: 20
+    },
+    header: {
+        transform: [{ translateY: "20%" }]
+    },
+    image: {
+        justifyContent: "center",
+        transform: [{ translateX: "-80%" }, { translateY: "-25%" }],
+        position: 'absolute'
+    },
+    im2: {
+        transform: [{ translateX: "-150%" }, { translateY: "-30%" }],
+        justifyContent: "center"
+    },
+    im3: {
+        transform: [{ translateX: "30%" }, { translateY: "-150%" }],
+        position: 'absolute'
+    },
+    headerText: {
+        position: "absolute",
+        fontSize: 50,
+        top: hp('20%'),
+        color: '#FFF9EC',
+        fontFamily: 'Poppins',
+        fontWeight: 'bold',
+        marginLeft: 10
+    },
+
+})
