bdecba4515243a24f6490b6208db3ce822fde46c
Nutchayaporn <54405491+Nutchayaporn@users.noreply.github.com>
2021-11-13 16:24:26 +0700

    History(Not complete)

--- FrontEnd/screen/History.js
+++ FrontEnd/screen/History.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, FlatList , Alert, Modal,Pressable} from 'react-native'
 import Bg from '../components/Pagebg'
+import HistoryGridTile from '../components/HistoryGridTile';
+import { useFonts } from 'expo-font';
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 
 const DATA1 = [
     {
-        title: 'อาการปวดหัว',
-        date: "Lorem Ipsum is  it",
+        title: 'Puad Hua',
+        date: "10/11/2020",
         symptom: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electr",
         diagnose: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electr",
         doctorAdvice: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electr",
@@ -15,25 +20,67 @@ const DATA1 = [
 
     },
 
+    {
+        title: 'Jeb Korrr',
+        date: "10/11/2020",
+        symptom: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electr",
+        diagnose: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electr",
+        doctorAdvice: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electr",
+        medicine: ["1", "2", "3"],
+        howToUse: ["HTU1", "HTU2", "HTU3"],
 
+    },
+    {
+        title: 'Puad Tong',
+        date: "10/11/2020",
+        symptom: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electr",
+        diagnose: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electr",
+        doctorAdvice: "Lorem Ipsum is  it to make a type specimen book. It has survived not only five centuries, but also the leap into electr",
+        medicine: ["1", "2", "3"],
+        howToUse: ["HTU1", "HTU2", "HTU3"],
 
+    },
 ];
 
 const History = () => {
+    const renderGridItem = (itemData) => {
+        return (
+            <HistoryGridTile
+                title={itemData.item.title}
+                date={itemData.item.date}
+            // color={itemData.item.color}
+            // onSelect={() => {
+            //   // เขียนโค้ดเพิ่ม
+            //   navigation.navigate("CategoryMeals", {categoryId : itemData.item.id , categoryTitle : itemData.item.title})
+            // }}
+            />
+
+        );
+    };
     return (
         <View style={styles.container}>
-            <Bg Text1='History' />
-
+            <Bg Text1='History' />    
+            <View style={styles.position}>
+                <FlatList data={DATA1} renderItem={renderGridItem} keyExtractor={item => item.title} numColumns={1} />
+            </View>
         </View>
     )
 }
 
 export default History
 
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         width: '100%',
         backgroundColor: '#6488e4',
+    },
+    position: {
+        flexDirection: 'column',
+        alignSelf: 'center',
+        position: 'absolute',
+        transform: [{ translateY: RFPercentage(20) }],
+        height: hp('70%')
     },
 })
