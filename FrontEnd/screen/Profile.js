238aa014a259c367a792a8b223ab23f0585e4ba8
PwrFr <54875724+PwrFr@users.noreply.github.com>
2021-11-17 14:01:12 +0700

    get Data for Profile

--- FrontEnd/screen/Profile.js
+++ FrontEnd/screen/Profile.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 import Bg from '../components/Pagebg'
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
@@ -8,24 +8,51 @@ import { MaterialIcons } from '@expo/vector-icons';
 import Btn from '../components/Button'
 import InfoBox from '../components/InfoBox';
 import InfoHalfBox from '../components/InfoHalfBox';
+import axios from "axios"
+
+
 const Profile = ({ navigation }) => {
+
+    let isMount = true
+    const [userinfo, setUserinfo] = useState({
+
+    })
+    useEffect(() => {
+        console.log("use eff");
+        const getUserInfo = async () => {
+            console.log("in");
+            if (isMount) {
+                console.log("getting");
+                const instance = axios.create({
+                    withCredentials: true
+                })
+                const local = "http://172.20.10.3:12345"
+                await instance.get(local + "/getuserinformation")
+                    .then(res => {
+                        setUserinfo(res.data.user_object)
+
+                    })
+                isMount = false
+            }
+        }
+        return (
+            getUserInfo()
+        )
+    }, [])
+
     return (
         <View style={styles.container}>
+
             <Bg Text1='Profile' />
             <View style={styles.position}>
-                <InfoBox f='Pawaris' m='Wongsaied' icon='person' titleTop='First Name ' titleMid='Last Name ' />
+                <InfoBox f={userinfo.firstname} m={userinfo.lastname} icon='person' titleTop='First Name ' titleMid='Last Name ' />
                 <KeyboardAwareScrollView style={{ marginTop: RFPercentage(1), height: hp('42%') }}>
                     <InfoHalfBox titleL='Birthday' infoL='04/07/2544' colorL='#309397' titleR='Sex' infoR='Male' colorR='#e46472' iconL='birthday-cake' iconR='transgender' />
                     <InfoHalfBox titleL='Address' infoL='653/1, RNP, Chalongkrung1, Ladkrabang BKK' colorL='#f9be7c' titleR='Phone' infoR='0954035514' colorR='#309397' iconL='address-book' iconR='phone' />
                     <InfoHalfBox titleL='Height' infoL='171' colorL='#e46472' titleR='Weight' infoR='62' colorR='#f9be7c' iconL='arrows-alt-h' iconR='arrows-alt-v' />
                     <InfoHalfBox titleL='Allergic' infoL='Peanut Butter' colorL='#309397' iconL='allergies' titleR='Congenital Disease' infoR='Diabetes' colorR='#e46472' iconR='disease' />
                     <InfoBox titleTop='ID' titleMid='Email' titleBot='Password' f='1100600434386' m='farrocker@outlook.co.th' l='FUCKU' icon='alternate-email' />
-                    {/* <Btn navigation={navigation} label='EDIT' color='#f9be7c' /> */}
-                    <TouchableOpacity style={{ ...styles.btn }} onPress={() => {
-                        // navigation.replace(props.to)
-                    }}>
-                        <Text style={{ fontSize: RFPercentage(3), fontFamily: 'Poppins', alignSelf: 'center' }}>EDIT</Text>
-                    </TouchableOpacity>
+                    <Btn navigation={navigation} label='EDIT' color='#f9be7c' />
                 </KeyboardAwareScrollView>
             </View>
         </View>
@@ -66,19 +93,5 @@ const styles = StyleSheet.create({
         fontFamily: 'Poppins',
         fontSize: RFPercentage(2),
 
-    },
-    btn: {
-        marginBottom: hp('5%'),
-        marginTop: hp('3%'),
-        fontFamily: 'Poppins',
-        alignSelf: 'center',
-        backgroundColor: '#f9be7c',
-        width: wp('80%'),
-        paddingVertical: 10,
-        borderRadius: 40,
-        shadowColor: "#000",
-        shadowOffset: { height: 7, width: 0 }, // IOS
-        shadowOpacity: 0.2, // IOS
-        shadowRadius: 3,
-    },
+    }
 })
