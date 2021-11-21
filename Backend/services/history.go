993f1baa676a2059cddb86cd3b3001ab4a45516a
kaolnwza <kaow_jijuy999@hotmail.com>
2021-11-21 18:23:41 +0700

    Squashed commit of the following:
    commit 36541fc2cc41d8e0ed25edcdd4f6815a94756888
    Author: kaolnwza <kaow_jijuy999@hotmail.com>
    Date:   Sun Nov 21 18:22:00 2021 +0700
    
        done get history
    
    commit 2b9896f82e2d9f3e63227c25d2c7c54cd9056ef5
    Author: kaolnwza <kaow_jijuy999@hotmail.com>
    Date:   Sun Nov 21 17:58:55 2021 +0700
    
        dfg
    
    commit d4f20c4bc47b4f31463034dad87101d5ff39ccff
    Merge: 52cb389 8e13f84
    Author: kaolnwza <kaow_jijuy999@hotmail.com>
    Date:   Sun Nov 21 17:08:02 2021 +0700
    
        Merge branch 'main' into kao2
    
    commit 52cb3896291303c1354345306b515601c22d7c6f
    Author: kaolnwza <kaow_jijuy999@hotmail.com>
    Date:   Sun Nov 21 17:07:08 2021 +0700
    
        Update Login.js

--- Backend/services/history.go
+++ Backend/services/history.go
@@ -11,29 +11,55 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+// func AddHistory_old(response http.ResponseWriter, request *http.Request) {
+// 	response.Header().Add("content-type", "application/json")
+// 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+// 	history_collection := client.Database(database).Collection("history")
+// 	user_collection := client.Database(database).Collection("user")
+// 	doctor_collection := client.Database(database).Collection("doctor")
+
+// 	var history_struct models.History
+// 	json.NewDecoder(request.Body).Decode(&history_struct)
+
+// 	//get user information
+// 	user_collection.FindOne(ctx, bson.M{"user_id": history_struct.User.User_id}).Decode(&history_struct.User)
+// 	//get doctor information
+// 	doctor_collection.FindOne(ctx, bson.M{"doctor_id": history_struct.Doctor.Doctor_id}).Decode(&history_struct.Doctor)
+
+// 	//find colletion range for make history_id
+// 	cursor, _ := history_collection.Find(ctx, bson.M{})
+// 	count := 0
+// 	defer cursor.Close(ctx)
+// 	for cursor.Next(ctx) {
+// 		count++
+// 	}
+// 	history_struct.History_id = count
+// 	history_struct.Date = time.Now()
+
+// 	fmt.Println("Done of add history")
+
+// 	history_collection.InsertOne(ctx, history_struct)
+
+// 	json.NewEncoder(response).Encode(history_struct)
+
+// }
+
 func AddHistory(response http.ResponseWriter, request *http.Request) {
 	response.Header().Add("content-type", "application/json")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	history_collection := client.Database(database).Collection("history")
-	user_collection := client.Database(database).Collection("user")
-	doctor_collection := client.Database(database).Collection("doctor")
 
 	var history_struct models.History
 	json.NewDecoder(request.Body).Decode(&history_struct)
 
-	//get user information
-	user_collection.FindOne(ctx, bson.M{"user_id": history_struct.User.User_id}).Decode(&history_struct.User)
-	//get doctor information
-	doctor_collection.FindOne(ctx, bson.M{"doctor_id": history_struct.Doctor.Doctor_id}).Decode(&history_struct.Doctor)
-
 	//find colletion range for make history_id
 	cursor, _ := history_collection.Find(ctx, bson.M{})
-	count := 0
+	history_id := 0
 	defer cursor.Close(ctx)
 	for cursor.Next(ctx) {
-		count++
+		history_id++
 	}
-	history_struct.History_id = count
+	history_struct.History_id = history_id
 	history_struct.Date = time.Now()
 
 	fmt.Println("Done of add history")
@@ -50,20 +76,36 @@ func GetHistory(response http.ResponseWriter, request *http.Request) {
 	history_collection := client.Database(database).Collection("history")
 
 	var get_data models.History
+	get_data.User_id = -1
 	json.NewDecoder(request.Body).Decode(&get_data)
+
+	if get_data.User_id == -1 {
+		get_data.User_id = userCookieId
+	}
+
 	var history_struct []models.History
 	//find user history
 
-	cursor, err := history_collection.Find(ctx, bson.M{"user.user_id": get_data.User_id})
+	cursor, err := history_collection.Find(ctx, bson.M{"user_id": get_data.User_id})
 	if err != nil {
 		fmt.Println(err)
 	}
+	doctor_collection := client.Database(database).Collection("doctor")
 	defer cursor.Close(ctx)
 	for cursor.Next(ctx) {
 		var temp models.History
 		cursor.Decode(&temp)
+		var docst models.Doctor
+		doctor_collection.FindOne(ctx, bson.M{"doctor_id": temp.Doctor.Doctor_id}).Decode(&docst)
+		docst.Doctor_Password = ""
+		temp.Doctor = docst
 		history_struct = append(history_struct, temp)
 	}
+
+	// var doctor_struct models.Doctor
+	// doctor_collection := client.Database(database).Collection("doctor")
+	// doctor_collection.FindOne()
+
 	fmt.Println("History")
 	json.NewEncoder(response).Encode(history_struct)
 }
--- FrontEnd/screen/History.js
+++ FrontEnd/screen/History.js
@@ -45,34 +45,28 @@ const DATA1 = [
 
 const History = () => {
 
-    let isMount = true
+
     const [userHistory, setuserHistory] = useState([])
     useEffect(() => {
         const getUserInfo = async () => {
-            if (isMount) {
-                console.log("history");
-                const instance = axios.create({
-                    withCredentials: true
+            await axios.get(global.local + "/gethistory")
+                .then(res => {
+                    setuserHistory(res.data)
                 })
-                await instance.get(global.local + "/gethistory")
-                    .then(res => {
-                        setuserHistory(res.data)
-                        // console.log(res.data)
 
-                    })
-                isMount = false
-            }
         }
+
         return (
             getUserInfo()
         )
-    }, [])
+    })
     const renderGridItem = (itemData) => {
         return (
             <HistoryGridTile
                 date={itemData.item.date}
                 title={itemData.item.symptom}
                 detail={itemData.item}
+                keyExtractor={itemData.history_id}
 
             // color={itemData.item.color}
             // onSelect={() => {
@@ -87,7 +81,7 @@ const History = () => {
         <View style={styles.container}>
             <Bg Text1='ประวัติการรักษา' />
             <View style={styles.position}>
-                <FlatList data={userHistory} renderItem={renderGridItem} keyExtractor={item => item.date} numColumns={1} />
+                <FlatList data={userHistory} renderItem={renderGridItem} keyExtractor={item => item._id} numColumns={1} />
             </View>
         </View>
     )
