0823d14557ba2ed435bf32949215b54c516dae12
Nutchayaporn <54405491+Nutchayaporn@users.noreply.github.com>
2021-11-22 14:44:21 +0700

    Add Doctor

--- Backend/services/doctor.go
+++ Backend/services/doctor.go
@@ -58,3 +58,26 @@ func GetDoctor(response http.ResponseWriter, request *http.Request) {
 	}
 	json.NewEncoder(response).Encode(getModel)
 }
+
+func AddDoctor(response http.ResponseWriter, request *http.Request) {
+
+	response.Header().Add("content-type", "application/json")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	doctor_collection := client.Database(database).Collection("doctor") // รับข้อมูลมาจาก DB
+	var doctor_structure models.Doctor
+	json.NewDecoder(request.Body).Decode(&doctor_structure) // รับข้อมูลจาก PM ลงมาใน structure
+
+
+
+	cursor, _ := doctor_collection.Find(ctx, bson.M{})
+	count := 0
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		count++
+	}
+	doctor_structure.Doctor_id = count
+	doctor_structure.Doctor_Password = EncodePassword(doctor_structure.Doctor_Password)
+
+	doctor_collection.InsertOne(ctx, doctor_structure) // inset ข้อมูลลง DB
+	json.NewEncoder(response).Encode(doctor_structure)      //แสดงใน PM
+}
